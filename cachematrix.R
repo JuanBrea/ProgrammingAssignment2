aef3fe709aed209c241728757eaf78876bc14c2a
Juan Brea <mortlach15@gmail.com>
2015-01-23 14:40:01 -0500

    Added explanatory comments and small refinement of the code

--- cachematrix.R
+++ cachematrix.R
@@ -1,13 +1,17 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
+## makecacheMatrix() will create a new object with interfaces to manipulate a matrix
+## Returns a list containing:
+## set_matrix(mtx) stores a new matrix in the object and sets it's inverse to NULL
+## get_matrix() return the matrix currently stored
+## set_inv(inv) allows for the matrix inverse to be saved. Intended to be invoked  from CacheSolve()
+## get_inv() returns the inverse of the current matrix
 makeCacheMatrix <- function(the_matrix = matrix()) {
+    #Creating for the first time. Set inv to null
     inv_matrix <- NULL
     
     set_matrix <-function (mtx) {
         the_matrix <<- mtx
+        # the matrix has changed. Set inv to NULL
         inv_matrix <<- NULL
     }
     
@@ -30,15 +34,24 @@ makeCacheMatrix <- function(the_matrix = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve(x) calculates the inverse of the matrix stored in object x
+## First tries to retrieve the inverse of the matrix 
+## If no inverse exists it retrieves the matrix stored, cacluates it's inverse and stores it in x
+## Returns the matrix invese or NULL if no matrix exists
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
     inv<-x$get_inv()
     if(!is.null(inv)){
+        #Found the inverse. No need to solve the matrix
         return(inv)
     }
     thematrix <-x$get_matrix()
+    if(is.null(thematrix)){
+        # No matrix found either
+        return (NULL)
+    }
+    # Calculate the inverse and store it for future use
     theinv <- solve(thematrix)
     x$set_inv(theinv)
     theinv
--- testcode_for_cachematrix.R
+++ testcode_for_cachematrix.R
@@ -1,22 +1,27 @@
 #test code for assignment 2
 
 source("cachematrix.R")
-cmtx <-makeCacheMatrix()
 
 #create matrix
 num_row_col <-10
-mtx <-matrix(rnorm(num_row_col^2, mean = 2, sd = 2), nrow = num_row_col, ncol = num_row_col)
+mtx <-matrix(rnorm(num_row_col^2, mean = 1, sd = 2), nrow = num_row_col, ncol = num_row_col)
+# Create matrix object
+cmtx <-makeCacheMatrix(mtx)
 
-cmtx <-makeCacheMatrix()
-cmtx$set_matrix(mtx)
-cacheSolve(cmtx)
-
-cmtx$get_matrix()
+# Should get NULL 
 cmtx$get_inv()
 
+#solve it and try again
+cacheSolve(cmtx)
+cmtx$get_inv()
+cmtx$get_matrix()
+
+
 #check if matrix multiplied by the Inverse == identity matrix
+# Diagonal is all '1'
 isIdent<-round(cmtx$get_inv()%*%cmtx$get_matrix(),2)
 isIdent
-cmtx$get_matrix()%*%iSIdent == cmtx$get_matrix()
+# Just for kicks. See if ident does not change original matrix
+cmtx$get_matrix()%*%isIdent == cmtx$get_matrix()
 
 
